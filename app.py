3e22bc1a4eee15c20a75bdd1ed220ae5d51dd68b
1731001 <126742269+1731001@users.noreply.github.com>
2023-05-16 11:26:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-
+st.checkbox('yes')
+st.button('Click')
+st.radio('Pick your gender',['Male','Female'])
+st.selectbox('Pick your gender',['Male','Female'])
+st.multiselect('choose a planet',['Jupiter','Mars','neptune'])
+st.select_slider('Pick a mark',['Bad','Good','Excellent'])
+st.slider('Pick a number', 0,50)
